a08cd0b9072c9e116277f52608ec85869ac1bf00
aleodoni <aleodoni@gmail.com>
2021-07-07 17:06:33 -0300

    aws: update scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,15 @@ source /home/ec2-user/.bash_profile
 
 cd /home/app/hello-aws
 
-#npm run start:prod
+# npm run start:prod
 
-pm2 start dist/main.js
+# pm2 start dist/main.js
+
+pm2 describe main > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start dist/main
+else
+  pm2 restart all
+fi;
